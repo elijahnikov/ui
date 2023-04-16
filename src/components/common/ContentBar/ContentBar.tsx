c10aa6aa775ad5d264d06856058fa46898505f8b
elijahnikov <posnikovelijah@gmail.com>
2023-04-16 23:53:22 +0100

    populating button page

--- src/components/common/ContentBar/ContentBar.tsx
+++ src/components/common/ContentBar/ContentBar.tsx
@@ -3,7 +3,11 @@ interface ContentBarProps {
 }
 
 const ContentBar = ({ children }: ContentBarProps) => {
-	return <div className="h-[100vh]">{children}</div>;
+	return (
+		<div className="flex mx-auto p-[20px] justify-center h-[100vh]">
+			<div className="w-[80%] mt-[20px]">{children}</div>
+		</div>
+	);
 };
 
 export default ContentBar;
--- src/pages/button.tsx
+++ src/pages/button.tsx
@@ -6,11 +6,30 @@ interface buttonProps {}
 const button = ({}: buttonProps) => {
 	return (
 		<Layout>
-			{/* <p>hello</p> */}
-			<h4>Primary</h4>
-			<Button intent={"primary"} loading={false}>
-				Primary
-			</Button>
+			<div>
+				<h3>Button</h3>
+				<p>
+					A control that triggers an action. Button labels should
+					express what action will occur when the user interacts with
+					it.
+				</p>
+			</div>
+			<div className="h-[40px]" />
+			<div>
+				<h4>Primary</h4>
+				<Button loading={false} intent="primary">
+					Primary
+				</Button>
+				<Button loading={false} intent="secondary">
+					Secondary
+				</Button>
+				<Button loading={false} intent="outline">
+					Outline
+				</Button>
+				<Button loading={false} intent="transparent">
+					Transparent
+				</Button>
+			</div>
 		</Layout>
 	);
 };
