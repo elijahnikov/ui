3f34c4cb23554735b51338915c5def1b35fcf7b2
Elijah Nikov <posnikovelijah@gmail.com>
2023-04-24 22:09:51 +0100

    removing comments

--- src/components/common/PageBrowser/PageBrowser.tsx
+++ src/components/common/PageBrowser/PageBrowser.tsx
@@ -12,13 +12,6 @@ const PageBrowser = ({ menuMap }: PageBrowserProps) => {
         <div className="hidden lg:block space-y-2 float-right right-0 fixed w-[20%] h-[90vh] text-left min-w-[20%] mx-auto p-[40px] min-h-[100vh]">
             <p className="font-semibold">On this page</p>
             <div className="block space-y-2 w-full">
-                {/* <AiFillCaretRight className="mt-[4px] mr-[4px]" />
-                <Link
-                    href={"#usage"}
-                    className="hover:underline dark:text-slate-400 cursor-pointer text-slate-700"
-                >
-                    Usage
-                </Link> */}
                 {menuMap.map((entry: MenuMap) => (
                     <div className="flex" key={entry.href}>
                         <AiFillCaretRight className="mt-[4px] mr-[4px]" />
