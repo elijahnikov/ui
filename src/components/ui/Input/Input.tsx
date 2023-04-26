0296919519743857d65577f3c476024fed35e9cc
Elijah Nikov <posnikovelijah@gmail.com>
2023-04-26 01:01:42 +0100

    working on input component, allowing users to add prefix and suffix

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,57 +1,67 @@
 import { cva, VariantProps } from "class-variance-authority";
 import React from "react";
 import { InputHTMLAttributes } from "react";
+import { IconType } from "react-icons";
 
-const input = cva(
-    [
-        "py-2 px-3",
-        "rounded-lg",
-        "border-gray-200 border-[1px] dark:border-gray-800",
-    ],
-    {
-        variants: {
-            intent: {
-                default: [
-                    "placeholder-gray-400 text-black dark:bg-black dark:text-white",
-                ],
-            },
-            size: {
-                base: "py-2",
-                sm: "text-sm",
-                large: "py-4",
-            },
-            fullWidth: {
-                true: "w-[100%]",
-            },
-            disabled: {
-                true: "placeholder-gray-400 dark:placeholder-gray-600 cursor-not-allowed pointer-events-none",
-            },
+const input = cva(["py-2 px-3", "rounded-lg", "border-[1px]"], {
+    variants: {
+        intent: {
+            default: [
+                "placeholder-ink-lighter text-black border-sky-light focus:outline-primary-base",
+                "dark:placeholder-sky-dark dark:bg-black dark:border-ink-dark dark:text-white",
+            ],
         },
-        compoundVariants: [
-            {
-                disabled: true,
-                intent: "default",
-                className: " bg-gray-200 dark:bg-gray-950 text-red-400",
-            },
-            {
-                disabled: false,
-                intent: "default",
-                className: "bg-white dark:bg-black",
-            },
-        ],
-        defaultVariants: {
-            size: "base",
+        size: {
+            base: "py-2",
+            sm: "text-sm",
+            large: "py-4",
+        },
+        fullWidth: {
+            true: "w-[100%]",
+        },
+        disabled: {
+            true: [
+                "placeholder-sky-base text-sky-dark border-sky-lighter bg-sky-lightest cursor-not-allowed pointer-events-none",
+                "dark:placeholder:ink-base dark:text-ink-base dark:border-ink-dark dark:bg-ink-darker",
+            ],
+        },
+        error: {
+            true: "border-red-400 dark:border-red-400",
+        },
+    },
+    compoundVariants: [
+        {
+            disabled: true,
             intent: "default",
+            className: " bg-gray-200 dark:bg-gray-950 text-red-400",
         },
-    }
-);
+        {
+            disabled: false,
+            intent: "default",
+            className: "bg-white dark:bg-black",
+        },
+    ],
+    defaultVariants: {
+        size: "base",
+        intent: "default",
+    },
+});
 
 interface InputProps
-    extends Omit<InputHTMLAttributes<HTMLInputElement>, "disabled" | "size">,
+    extends Omit<
+            InputHTMLAttributes<HTMLInputElement>,
+            "disabled" | "size" | "error" | "prefix" | "suffix"
+        >,
         VariantProps<typeof input> {
     placeholder?: string;
     label?: string;
+    caption?: string;
+    error?: boolean;
+    errorText?: string;
     disabled?: boolean;
+    prefix?: IconType | string;
+    suffix?: IconType | string;
+    clearable?: boolean;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
@@ -62,6 +72,10 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             className,
             size,
             disabled,
+            error,
+            errorText,
+            prefix: Prefix,
+            suffix: Suffix,
             placeholder,
             fullWidth,
             label,
@@ -70,21 +84,45 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         ref
     ) => {
         return (
-            <input
-                ref={ref}
-                disabled={disabled}
-                placeholder={placeholder}
-                {...props}
-                className={input({
-                    intent,
-                    size,
-                    disabled,
-                    className,
-                    fullWidth,
-                })}
-            >
-                {children}
-            </input>
+            <>
+                {label && (
+                    <div className="relative top-[-5px] ml-1">
+                        <label className="text-sm text-black dark:text-white">
+                            {label}
+                        </label>
+                    </div>
+                )}
+                <div
+                    className={`${
+                        (Prefix || Suffix) && "flex"
+                    } w-[100%] text-left`}
+                >
+                    {Prefix && (
+                        <div className="rounded-l-lg relative z-10 bg-white dark:bg-black left-2 border-[1px] border-sky-light dark:border-ink-dark border-r-[1px] py-2 px-3 text-sm">
+                            <p className="mt-[2px] text-slate-400">
+                                {(Prefix as IconType) && <Prefix />}
+                                {typeof Prefix === "string" && String(Prefix)}
+                            </p>
+                        </div>
+                    )}
+                    <input
+                        ref={ref}
+                        disabled={disabled}
+                        placeholder={placeholder}
+                        {...props}
+                        className={input({
+                            intent,
+                            size,
+                            disabled,
+                            error,
+                            className,
+                            fullWidth,
+                        })}
+                    >
+                        {children}
+                    </input>
+                </div>
+            </>
         );
     }
 );
--- src/pages/input.tsx
+++ src/pages/input.tsx
@@ -1,10 +1,14 @@
 import Layout from "@/components/common/Layout/Layout";
 import PageHeader from "@/components/common/PageHeader/PageHeader";
-import React from "react";
+import Input from "@/components/ui/Input/Input";
+import React, { useEffect, useState } from "react";
+import { Bs0Circle } from "react-icons/bs";
 
 interface InputPageProps {}
 
 const InputPage = ({}: InputPageProps) => {
+    const [value, setValue] = useState<string>("");
+
     return (
         <Layout title="Input - Supercrumble UI">
             <div className="mb-[40px]">
@@ -12,6 +16,22 @@ const InputPage = ({}: InputPageProps) => {
                     title="Input"
                     description="An input component used to retrieve user input in the form of a text field"
                 />
+                <div className="mt-[20px] h-[500px] block">
+                    <Input
+                        label="Username"
+                        placeholder="Enter your username..."
+                        size={"base"}
+                    />
+                    <div className="h-[50px]" />
+                    <Input
+                        label={"Username"}
+                        prefix={"One"}
+                        fullWidth
+                        placeholder="Base"
+                        size={"base"}
+                    />
+                </div>
+                {value}
             </div>
         </Layout>
     );
