75e006b3fcde6ffcd23547ff9359ea2b70a1c240
Elijah Nikov <posnikovelijah@gmail.com>
2023-05-09 23:51:56 +0100

    using jsx namespacing to add to triple column component

--- src/components/ui/TripleColumn/TripleColumn.tsx
+++ src/components/ui/TripleColumn/TripleColumn.tsx
@@ -1,26 +1,33 @@
 import React from "react";
 
 interface TripleColumnProps {
-    left: React.ReactNode;
-    middle: React.ReactNode;
-    right: React.ReactNode;
+    children: React.ReactNode;
 }
 
-const TripleColumn = ({ left, middle, right }: TripleColumnProps) => {
+const TripleColumn = ({ children }: TripleColumnProps) => {
     return (
         <div className="h-[100%]">
             <div className="mx-auto">
-                <div className="absolute flex w-full">
-                    {/* LEFT */}
-                    <div></div>
-                    {/* MIDDLE */}
-                    <div></div>
-                    {/* RIGHT */}
-                    <div></div>
-                </div>
+                <div className="absolute flex w-full">{children}</div>
             </div>
         </div>
     );
 };
+
+const Left = ({ children }: { children: React.ReactNode }) => {
+    return <div>{children}</div>;
+};
+
+const Middle = ({ children }: { children: React.ReactNode }) => {
+    return <div>{children}</div>;
+};
+
+const Right = ({ children }: { children: React.ReactNode }) => {
+    return <div>{children}</div>;
+};
+
+TripleColumn.Left = Left;
+TripleColumn.Middle = Middle;
+TripleColumn.Right = Right;
 
 export default TripleColumn;
