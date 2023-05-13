d9db5e396512ce23b6ea51085bd5353150dde093
Elijah Nikov <posnikovelijah@gmail.com>
2023-05-14 00:19:37 +0100

    major work completed on select component

--- src/components/ui/Select/Select.tsx
+++ src/components/ui/Select/Select.tsx
@@ -1,110 +1,114 @@
-import { VariantProps, cva } from "class-variance-authority";
-import React, { useRef } from "react";
-import { SelectHTMLAttributes } from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
+import * as PopoverPrimitive from "@radix-ui/react-popover";
+import { BsChevronDown } from "react-icons/bs";
 
-const select = cva([], {
-    variants: {
-        intent: {
-            default: [],
-        },
-        size: {
-            base: "",
-            sm: "",
-            large: "",
-        },
-        fullWidth: {
-            true: "",
-        },
-        disabled: {
-            true: "",
-        },
-        error: {
-            true: "",
-        },
-    },
-    defaultVariants: {
-        size: "base",
-        intent: "default",
-    },
-});
-
-interface Option {
+type Option = {
     value: string;
     label: string;
-}
+};
 
-interface SelectProps
-    extends Omit<
-            SelectHTMLAttributes<HTMLSelectElement>,
-            "disabled" | "size" | "error"
-        >,
-        VariantProps<typeof select> {
-    label?: string;
-    caption?: string;
-    error?: boolean;
-    errorText?: string;
+interface SelectProps {
+    selectName?: string;
     disabled?: boolean;
-    defaultValue?: string;
-    options: Option[];
+    value: string;
+    setValue: (value: string) => void;
+    label?: string;
+    children: JSX.Element | JSX.Element[];
 }
 
-const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
-    ({
-        intent,
-        children,
-        className,
-        size,
-        disabled,
-        error = false,
-        errorText,
-        fullWidth,
-        label,
-        options,
-        defaultValue,
-        ...props
-    }) => {
-        const selectRef = useRef<HTMLSelectElement | null>(null);
+interface SelectItemProps {
+    children: React.ReactNode;
+    value: string;
+}
 
-        return (
-            <>
-                {label && (
-                    <div className="relative top-[-5px] ml-1">
-                        <label className="text-sm text-black dark:text-white">
-                            {label}
-                        </label>
-                    </div>
-                )}
-                <div>
-                    <select
-                        defaultValue={
-                            defaultValue ? defaultValue : options[0].value
-                        }
-                        ref={selectRef}
-                        disabled={disabled}
-                        {...props}
-                        className={select({
-                            intent,
-                            size,
-                            disabled,
-                            error,
-                            className,
-                            fullWidth,
-                        })}
-                    >
-                        {options.map((option: Option, i: number) => {
-                            return (
-                                <option key={i} value={option.value}>
-                                    {option.label}
-                                </option>
-                            );
-                        })}
-                    </select>
+const Select = ({
+    selectName,
+    disabled,
+    label,
+    value,
+    setValue,
+    children,
+}: SelectProps) => {
+    const [open, setOpen] = useState<boolean>(false);
+    return (
+        <>
+            {label && (
+                <div className="relative top-[-5px] ml-1">
+                    <label className="text-sm text-black dark:text-white">
+                        {label}
+                    </label>
                 </div>
-            </>
-        );
-    }
-);
+            )}{" "}
+            <PopoverPrimitive.Root
+                open={open}
+                onOpenChange={() => setOpen(!open)}
+            >
+                <PopoverPrimitive.Trigger
+                    className={`${
+                        open
+                            ? "border-primary-base"
+                            : "border-sky-light dark:border-slate-800"
+                    } absolute flex w-[200px] py-2 px-3 text-left border-[2px] rounded-lg`}
+                >
+                    <p className="w-[90%]">
+                        {value !== "" ? value : selectName}
+                    </p>
+                    <BsChevronDown className="mt-1 fill-ink-light" />
+                </PopoverPrimitive.Trigger>
+                <PopoverPrimitive.Portal>
+                    <PopoverPrimitive.Content className="border-sky-light dark:border-slate-800 space-y-2 mt-2 w-[200px] rounded-lg p-[10px] border-[2px]">
+                        {React.Children.map(children, (child) => {
+                            return React.cloneElement(child, {
+                                selectedValue: value,
+                                setValue,
+                                setOpen,
+                                open,
+                            });
+                        })}
+                    </PopoverPrimitive.Content>
+                </PopoverPrimitive.Portal>
+            </PopoverPrimitive.Root>
+        </>
+    );
+};
 
-Select.displayName = "Select";
+const SelectItem = ({
+    value,
+    children,
+    selectedValue,
+    setValue,
+    open,
+    setOpen,
+}: {
+    value: string;
+    children: React.ReactNode;
+    selectedValue?: string;
+    setValue?: (value: string) => void;
+    open?: boolean;
+    setOpen?: (value: boolean) => void;
+}) => {
+    return (
+        <div
+            onClick={() => {
+                setValue && setValue(value);
+                setOpen && setOpen(!open);
+            }}
+            className={`${
+                selectedValue === value && "bg-sky-lighter dark:bg-slate-800"
+            } cursor-pointer hover:bg-sky-lighter dark:hover:bg-slate-800 rounded-md p-1`}
+            key={value}
+        >
+            <p className="ml-1">{children}</p>
+        </div>
+    );
+};
 
-export default Select;
+const SelectSearch = () => {
+    return <h1>search</h1>;
+};
+
+const SelectContent = ({ children }: { children: React.ReactNode }) => {
+    return <>{children}</>;
+};
+
+export { Select, SelectContent, SelectItem, SelectSearch };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,35 @@
 import Layout from "@/components/common/Layout/Layout";
 import InputArea from "@/components/ui/InpuArea/InputArea";
-import Select from "@/components/ui/Select/Select";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectSearch,
+} from "@/components/ui/Select/Select";
+
 import TripleColumn from "@/components/ui/TripleColumn/TripleColumn";
 import { useState } from "react";
 
 export default function Home() {
     const [clearableText, setClearableText] = useState<string>("");
-
+    const [value, setValue] = useState("Banana");
     return (
         <Layout>
-            <h1>hello</h1>
             <Select
-                defaultValue="test2"
-                options={[
-                    { value: "test1", label: "Test1" },
-                    { value: "test2", label: "Test2" },
-                    { value: "test3", label: "Test3" },
-                ]}
-            />
+                value={value}
+                setValue={setValue}
+                label="Select a fruit"
+                selectName="Fruits"
+            >
+                <SelectSearch />
+                <SelectContent>
+                    <SelectItem value="Apple">Apple</SelectItem>
+                    <SelectItem value="Banana">Banana</SelectItem>
+                    <SelectItem value="Orange">Orange</SelectItem>
+                    <SelectItem value="Pear">Pear</SelectItem>
+                    <SelectItem value="Strawberry">Strawberry</SelectItem>
+                </SelectContent>
+            </Select>
         </Layout>
     );
 }
