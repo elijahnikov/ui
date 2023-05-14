ad8623b5bd37ccb50642609374bf1a21fdc75ac2
Elijah Nikov <posnikovelijah@gmail.com>
2023-05-14 23:43:58 +0100

    populating example page for select

---
+++ src/maps/component/select.ts
@@ -0,0 +1,44 @@
+import { MenuMap } from "./types";
+
+export const selectMenuMap: MenuMap[] = [
+    {
+        label: "Usage",
+        href: "#usage",
+    },
+    {
+        label: "Label",
+        href: "#label",
+    },
+];
+
+export const selectCode = {
+    usage: `import { Select, SelectItem } from "@/components/ui/Select/Select";
+
+const [selectedFruit, setSelectedFruit] = useState<string>("");
+
+<Select
+    value={selectedFruit}
+    setValue={setSelectedFruit}
+    placeholder="Fruits"
+>
+    <SelectItem value="Apple">Apple</SelectItem>
+    <SelectItem value="Banana">Banana</SelectItem>
+    <SelectItem value="Orange">Orange</SelectItem>
+    <SelectItem value="Pear">Pear</SelectItem>
+    <SelectItem value="Strawberry">Strawberry</SelectItem>
+</Select>`,
+
+    label: `import { Select, SelectItem } from "@/components/ui/Select/Select";
+
+const [yesNo, setYesNo] = useState<string>("");
+
+<Select
+    label="Label for select"
+    value={yesNo}
+    setValue={setYesNo}
+    placeholder="Agree"
+>
+    <SelectItem value="Yes">Yes</SelectItem>
+    <SelectItem value="No">No</SelectItem>
+</Select>`,
+};
