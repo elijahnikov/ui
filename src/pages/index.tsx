b0ccd2c20901db06d264549187090dcd9c781887
Elijah Nikov <posnikovelijah@gmail.com>
2023-05-16 23:40:40 +0100

    creating tooltip component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,21 @@
 import Layout from "@/components/common/Layout/Layout";
 import { Badge, BadgeIcon, BadgeLabel } from "@/components/ui/Badge/Badge";
+import Button from "@/components/ui/Button/Button";
 import Tabs from "@/components/ui/Tabs/Tabs";
+import Tooltip from "@/components/ui/Tooltip/Tooltip";
 import { BiFile, BiGitBranch } from "react-icons/bi";
-import { BsGithub } from "react-icons/bs";
+import { BsGithub, BsPlus } from "react-icons/bs";
 
 export default function Home() {
     return (
         <Layout>
-            <div className="space-y-5">
-                <Badge>
-                    <BadgeIcon icon={BiGitBranch} />
-                    <BadgeLabel>component/badge</BadgeLabel>
-                    <BadgeIcon icon={BiGitBranch} />
-                </Badge>
+            <div>
+                <Tooltip>
+                    <Tooltip.Trigger>
+                        <Button>Hello</Button>
+                    </Tooltip.Trigger>
+                    <Tooltip.Content>123</Tooltip.Content>
+                </Tooltip>
             </div>
         </Layout>
     );
