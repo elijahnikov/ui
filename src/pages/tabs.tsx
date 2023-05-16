a2a13971fd2fe13b8b825e0664953bc166e6403e
Elijah Nikov <posnikovelijah@gmail.com>
2023-05-16 01:01:43 +0100

    populating tabs example page, editing input component

--- src/pages/tabs.tsx
+++ src/pages/tabs.tsx
@@ -1,5 +1,12 @@
+import ExampleBlock from "@/components/common/ExampleBlock/ExampleBlock";
+import GitHubRedirect from "@/components/common/GitHubRedirect/GitHubRedirect";
 import Layout from "@/components/common/Layout/Layout";
 import PageHeader from "@/components/common/PageHeader/PageHeader";
+import Button from "@/components/ui/Button/Button";
+import CodeBlock from "@/components/ui/CodeBlock/CodeBlock";
+import Input from "@/components/ui/Input/Input";
+import Tabs from "@/components/ui/Tabs/Tabs";
+import { tabsCode } from "@/maps/component/tabs";
 import React from "react";
 
 interface TabsPageProps {}
@@ -13,6 +20,68 @@ const TabsPage = ({}: TabsPageProps) => {
                     description="Sections of content that are displayed one at a time, depending on which tab is selected."
                 />
             </div>
+            <div>
+                <a
+                    className="relative h-[20px] invisible top-[-80px]"
+                    id="usage"
+                ></a>
+                <h4 className="mb-[30px]">Usage</h4>
+                <ExampleBlock>
+                    <Tabs defaultTab="account">
+                        <Tabs.List>
+                            <Tabs.Trigger value="account">Account</Tabs.Trigger>
+                            <Tabs.Trigger value="password">
+                                Password
+                            </Tabs.Trigger>
+                        </Tabs.List>
+                        <Tabs.Content value="account">
+                            <div className="p-5 border-[1px] dark:border-slate-800 rounded-md mt-2">
+                                <h3>Account</h3>
+                                <p>
+                                    {`Make changes to your account here. Click
+                                    save when you're done.`}
+                                </p>
+                                <div className="mt-4">
+                                    <Input fullWidth label="Name" />
+                                    <div className="mt-4">
+                                        <Input fullWidth label="Username" />
+                                    </div>
+                                    <Button className="mt-5">
+                                        Save changes
+                                    </Button>
+                                </div>
+                            </div>
+                        </Tabs.Content>
+                        <Tabs.Content value="password">
+                            <div className="p-5 border-[1px] dark:border-slate-800 rounded-md mt-2">
+                                <h3>Password</h3>
+                                <p>
+                                    {`Change your password here. After saving, you'll be logged out.`}
+                                </p>
+                                <div className="mt-4">
+                                    <Input
+                                        type="password"
+                                        fullWidth
+                                        label="Current password"
+                                    />
+                                    <div className="mt-4">
+                                        <Input
+                                            type="password"
+                                            fullWidth
+                                            label="New password"
+                                        />
+                                    </div>
+                                    <Button className="mt-5">
+                                        Save changes
+                                    </Button>
+                                </div>
+                            </div>
+                        </Tabs.Content>
+                    </Tabs>
+                </ExampleBlock>
+                <CodeBlock code={tabsCode.usage} />
+            </div>
+            <GitHubRedirect page="Tabs" href="" />
         </Layout>
     );
 };
