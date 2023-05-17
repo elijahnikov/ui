e4ae36719b867cf0c1bce806aea398f06072e652
Elijah Nikov <posnikovelijah@gmail.com>
2023-05-17 23:00:53 +0100

    spinner component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "@/components/common/Layout/Layout";
 import { Badge, BadgeIcon, BadgeLabel } from "@/components/ui/Badge/Badge";
 import Button from "@/components/ui/Button/Button";
+import Spinner from "@/components/ui/Spinner/Spinner";
 import Tabs from "@/components/ui/Tabs/Tabs";
 import Tooltip from "@/components/ui/Tooltip/Tooltip";
 import { BiFile, BiGitBranch } from "react-icons/bi";
@@ -10,12 +11,7 @@ export default function Home() {
     return (
         <Layout>
             <div className="mt-[20px]">
-                <Tooltip>
-                    <Tooltip.Trigger>
-                        <Button>Hello</Button>
-                    </Tooltip.Trigger>
-                    <Tooltip.Content>123</Tooltip.Content>
-                </Tooltip>
+                <Spinner />
             </div>
         </Layout>
     );
