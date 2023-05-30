79627a45ba3aa13f8f71c1341eaa066b1f97d8a7
Elijah Nikov <posnikovelijah@gmail.com>
2023-05-30 23:37:45 +0100

    initial setup of modal using radix ui dialog primitive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Layout from "@/components/common/Layout/Layout";
 import Accordion from "@/components/ui/Accordion/Accordion";
+import Button from "@/components/ui/Button/Button";
+import Modal from "@/components/ui/Dialog/Dialog";
 import Input from "@/components/ui/Input/Input";
 import Kbd from "@/components/ui/Kbd/Kbd";
 import Progress from "@/components/ui/Progress/Progress";
@@ -8,11 +10,16 @@ import Spinner from "@/components/ui/Spinner/Spinner";
 export default function Home() {
     return (
         <Layout>
-            <Input
-                placeholder="Search Supercrumble UI"
-                className="w-[400px]"
-                kbd={<Kbd shift>/</Kbd>}
-            />
+            <Modal>
+                <Modal.Trigger>Click me</Modal.Trigger>
+                <Modal.Content>
+                    <Modal.Title>Hello</Modal.Title>
+                    <Input />
+                    <Modal.Close>
+                        <Button>Close</Button>
+                    </Modal.Close>
+                </Modal.Content>
+            </Modal>
         </Layout>
     );
 }
