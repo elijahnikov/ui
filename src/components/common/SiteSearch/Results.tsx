1915ed0f2ab19f9c61927e29598b8b343a9bbaf4
Elijah Nikov <posnikovelijah@gmail.com>
2023-07-07 22:04:20 +0100

    reindenting

--- src/components/common/SiteSearch/Results.tsx
+++ src/components/common/SiteSearch/Results.tsx
@@ -4,69 +4,73 @@ import { useRouter } from "next/router";
 import React, { useCallback, useEffect, useState } from "react";
 
 type ComponentResult = {
-  id: number;
-  title: string;
-  value: string;
-  href: string;
-  type: string;
+    id: number;
+    title: string;
+    value: string;
+    href: string;
+    type: string;
 };
 
 interface ResultsProps {
-  componentResults: ComponentResult[];
-  inputReference: React.MutableRefObject<HTMLInputElement | null>;
+    componentResults: ComponentResult[];
+    inputReference: React.MutableRefObject<HTMLInputElement | null>;
 }
 
 const Results = ({ componentResults, inputReference }: ResultsProps) => {
-  const [cursor, setCursor] = useState<number>(0);
-  const [results, setResults] = useState<ComponentResult[]>(componentResults);
-  const router = useRouter();
+    const [cursor, setCursor] = useState<number>(0);
+    const [results, setResults] = useState<ComponentResult[]>(componentResults);
+    const router = useRouter();
 
-  useKeyPress("Escape", () => {
-    if (inputReference.current) {
-      inputReference.current.value = "";
-      inputReference.current.blur();
-    }
-    setResults([]);
-  });
-  useKeyPress("ArrowUp", () => {
-    if (cursor > 0) setCursor(cursor - 1);
-  });
-  useKeyPress("ArrowDown", () => {
-    if (cursor < componentResults.length - 1) setCursor(cursor + 1);
-  });
-  useKeyPress("Enter", () => router.push(componentResults[cursor].href));
+    useKeyPress("Escape", () => {
+        if (inputReference.current) {
+            inputReference.current.value = "";
+            inputReference.current.blur();
+        }
+        setResults([]);
+    });
+    useKeyPress("ArrowUp", () => {
+        if (cursor > 0) setCursor(cursor - 1);
+    });
+    useKeyPress("ArrowDown", () => {
+        if (cursor < componentResults.length - 1) setCursor(cursor + 1);
+    });
+    useKeyPress("Enter", () => router.push(componentResults[cursor].href));
 
-  useEffect(() => {
-    setResults(componentResults);
-  }, [componentResults]);
+    useEffect(() => {
+        setResults(componentResults);
+    }, [componentResults]);
 
-  return (
-    <>
-      {results.length > 0 && (
-        <div className="absolute bg-white border-[1px] rounded-lg mt-1 p-2 w-[100%]">
-          {componentResults.map((component, i) => (
-            <div
-              className={`${
-                cursor === i
-                  ? "bg-primary-lightest"
-                  : "hover:bg-primary-lightest"
-              } text-left cursor-pointer p-2  rounded-md`}
-              key={component.id}
-              onMouseEnter={() => setCursor(i)}
-            >
-              <Link href={component.href}>
-                <p className="text-primary-dark">{component.title}</p>
-                <p className="text-sm text-ink-dark">{component.type}</p>
-              </Link>
-            </div>
-          ))}
-          <p className="text-sm mt-2 text-ink-light">
-            Use arrow keys and enter to navigate
-          </p>
-        </div>
-      )}
-    </>
-  );
+    return (
+        <>
+            {results.length > 0 && (
+                <div className="absolute bg-white border-[1px] rounded-lg mt-1 p-2 w-[100%]">
+                    {componentResults.map((component, i) => (
+                        <div
+                            className={`${
+                                cursor === i
+                                    ? "bg-primary-lightest"
+                                    : "hover:bg-primary-lightest"
+                            } text-left cursor-pointer p-2  rounded-md`}
+                            key={component.id}
+                            onMouseEnter={() => setCursor(i)}
+                        >
+                            <Link href={component.href}>
+                                <p className="text-primary-dark">
+                                    {component.title}
+                                </p>
+                                <p className="text-sm text-ink-dark">
+                                    {component.type}
+                                </p>
+                            </Link>
+                        </div>
+                    ))}
+                    <p className="text-sm mt-2 text-ink-light">
+                        Use arrow keys and enter to navigate
+                    </p>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default Results;
